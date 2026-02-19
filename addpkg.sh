76a6c99a32af88339ceb6ce6002523651cae63bd
Diego Fronza <darkfronza@gmail.com>
2026-02-19 16:58:25 -0300

    Fix sed invokation on Linux

--- addpkg.sh
+++ addpkg.sh
@@ -9,19 +9,38 @@ pkg="$1"
 pkg_with_version=$(basename $(ls -1 pkgs/${pkg}/${pkg}_* | sort -r | head -n 1) .nix)
 nix_entry="pkgs.callPackage ./pkgs/${pkg}/${pkg_with_version}.nix { }"
 
+unameOut="$(uname -s)"
+OS=Linux
+
+case "${unameOut}" in
+Darwin*) OS=Mac ;;
+esac
+
+if [ "$OS" = "Mac" ]; then
+    xsed() {
+        sed -i "" "$@"
+    }
+else
+    xsed() {
+        sed -i "$@"
+    }
+fi
+
 if ! grep -Eqs "${pkg_with_version}[[:blank:]]*=" default.nix; then
     echo "Adding new package entry to default.nix: pkg=$pkg  version=$version"
 
-    sed -i "" "/<package-list>/a\\
+    xsed "/<package-list>/a\\
   ${pkg_with_version} = ${nix_entry}" default.nix
 fi
 
 # patch default package name to point to the latest version
 if ! grep -Eqs "${pkg}[[:blank:]]*=" default.nix; then
     echo "Adding default $pkg package entry..."
-    sed -i "" "/<package-list>/a\\
+
+    xsed "/<package-list>/a\\
   ${pkg} = ${nix_entry}" default.nix
 else
     echo "Updating $pkg to point to new version $version"
-    sed -i "" "s#^.*${pkg} *=.*\$#  ${pkg} = ${nix_entry}#" default.nix
+
+    xsed "s#^.*${pkg} *=.*\$#  ${pkg} = ${nix_entry}#" default.nix
 fi
