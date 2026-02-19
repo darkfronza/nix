17e56290c4fd836d64083f225b838271e89f4aaa
Diego Fronza <darkfronza@gmail.com>
2026-02-19 17:34:35 -0300

    fix package name and missing ;'

--- addpkg.sh
+++ addpkg.sh
@@ -7,7 +7,8 @@
 
 pkg="$1"
 pkg_with_version=$(basename $(ls -1 pkgs/${pkg}/${pkg}_* | sort -Vr | head -n 1) .nix)
-nix_entry="pkgs.callPackage ./pkgs/${pkg}/${pkg_with_version}.nix { }"
+pkg_name="$(echo ${pkg_with_version} | tr '.' '_')"
+nix_entry="pkgs.callPackage ./pkgs/${pkg}/${pkg_with_version}.nix { };"
 
 unameOut="$(uname -s)"
 OS=Linux
@@ -30,7 +31,7 @@ if ! grep -Eqs "${pkg_with_version}[[:blank:]]*=" default.nix; then
     echo "Adding new package entry to default.nix: pkg=$pkg  version=$version"
 
     xsed "/<package-list>/a\\
-  ${pkg_with_version} = ${nix_entry}" default.nix
+  ${pkg_name} = ${nix_entry}" default.nix
 fi
 
 # patch default package name to point to the latest version
